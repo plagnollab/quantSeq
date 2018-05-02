994661784d90510c8bc41b9352f2954d6b4f1bd5
Jack Humphrey <jack@eduroam-int-dhcp-97-156-207.ucl.ac.uk>
2018-05-02 15:31:42 +0100

    separated and cleaned functions and put into /R. added example script in /scripts

---
+++ R/DRIMSeq_functions.R
@@ -0,0 +1,173 @@
+# library(DRIMSeq) 
+# library(stageR)
+# library(dplyr)
+# library(tidyr)
+# library(plyr)
+# 
+
+
+
+#make feature ID unique and reformat the data
+featid <- function(df){
+  df$feature_id <- paste(df$gene_name, df$site_pos, sep = "_")
+  dat <- data.frame(gene_id = df$gene_id, feature_id = df$feature_id)
+  # create count matrix with meta data columns
+  dat[,3:22] <- df[,c(10:19, 21:30)]
+  return(dat)
+}
+
+
+# run DRIMSeq
+DRIMrun <- function(gr, df){
+  d <- DRIMSeq::dmDSdata(counts = df, samples = gr) 
+  d <- DRIMSeq::dmFilter(d, min_samps_gene_expr = 5, min_samps_feature_expr = 5,
+                min_gene_expr = 10, min_feature_expr = 4)
+  set.seed(1337)
+  design_full <- model.matrix(~ group, data = d@samples)
+  d <- DRIMSeq::dmPrecision(d, design = design_full)
+  d <- DRIMSeq::dmFit(d, design = design_full, verbose = 1) 
+  d <- DRIMSeq::dmTest(d, coef =  colnames(design_full)[2]) 
+  # return d?
+  return(d)
+}
+
+
+# for a single gene, plot the proportions of polyA sites
+plotPolyA <- function(geneid, data, DRIMSeq_res){
+  
+  # get feature IDs for x axis
+  gene_data <- dplyr::filter(data, gene_id == geneid)
+  
+  if (gene_data$strand[1] == '+'){
+    featid <- sort(gene_data$feature_id, decreasing = FALSE)
+    sitepos <- sort(gene_data$site_pos, decreasing = FALSE)
+  } else {
+    featid <- sort(gene_data$feature_id, decreasing = TRUE)
+    sitepos <- sort(gene_data$site_pos, decreasing = TRUE)
+  }
+  
+  # extract per gene p value to add to plot title
+  gene_res <- DRIMSeq::results(DRIMSeq_res, level = "gene") 
+  gene_pvalue <- dplyr::filter(gene_res, gene_id == geneid)$pvalue
+  
+  gene_pvalue <- signif(gene_pvalue, digits = 3)
+  
+  # make plot
+  p <- DRIMSeq::plotProportions(DRIMSeq_res, gene_id = geneid, group_variable = "group")
+  
+  # add labels and title
+  p <- p + scale_x_discrete(limits = featid) + xlab("") + labs(title = unique(gene_data$gene_name), subtitle = paste0("Gene P = ", gene_pvalue) )
+  
+  return(p)
+}
+
+
+# 
+StageRun <- function(d, case_condition, alpha = 0.05){
+  
+  # get gene wide p values
+  pScreen <- DRIMSeq::results(d, level = "gene")$pvalue
+  
+  names(pScreen) <- DRIMSeq::results(d)$gene_id
+  
+  # get polyA site specific p values
+  pConfirmation <- matrix( DRIMSeq::results(d, level = "feature")$pvalue, ncol = 1)
+  
+  rownames(pConfirmation) <- DRIMSeq::results(d, level = "feature")$feature_id
+  
+  # map genes to features
+  tx2gene <- DRIMSeq::results(d, level = "feature")[, c("feature_id", "gene_id")]
+  
+  # load into stageR object
+  stageRObj <- stageR::stageRTx(pScreen = pScreen,
+                                pConfirmation = pConfirmation,
+                                pScreenAdjusted = FALSE, 
+                                tx2gene = tx2gene)
+  
+  #perform stage-wise adjustment for differential transcript usage
+  # alpha? probably should set
+  stageRObj <- stageR::stageWiseAdjustment(object = stageRObj, method = "dtu",
+                                   alpha = alpha)
+  
+  # adjust p value
+  # again - all genes?
+  padj <- stageR::getAdjustedPValues(stageRObj, order = TRUE,
+                             onlySignificantGenes = FALSE)
+  
+  # 
+  coeff <- DRIMSeq::coefficients(d, level="feature")
+  
+  # change is effect of the case condition 
+  case_column <- paste0("group", case_condition)
+  coe <- data.frame(txID = coeff$feature_id, change = coeff[[case_column]])
+  
+  results <- inner_join( padj, coe, by = "txID")
+  
+  # full is created from inner_join on multiple datasets - probably just have a general list of genes instead
+  #full2 <- full 
+  #full2 <- rename(full2, "txID" = "feature_id")
+  
+  # add in padj and r
+  #TranscriptWPval <- merge(full2, padj, by="txID")
+  
+  #TranscriptWAll <- merge(TranscriptWPval, coe, by = "txID")
+  
+  #Temp <- dplyr::select(TranscriptWAll, gene_id, gene_name, txID, chr, strand, site_pos:change) 
+  
+  return( results )
+  
+  #return(Temp)
+}
+# 
+# GetSites <- function(agR){
+#   
+#   AgaResults <- dplyr::select(agR, gene_id, gene_name, txID, gene, transcript, change)
+#   AgaResults$sign <- sign(AgaResults$change)
+#   
+#   # unnecessary?
+#   AgaResults$change <- AgaResults$change
+#   
+#   genes <- unique(agR$gene_name, incomparables = FALSE)
+#   
+#   # initialise empty data frames
+#   Temp2 <- data.frame(txID=NA, PtD=NA)
+#   MostDistal <- data.frame(txID=NA, PtD=NA)
+#   
+#   # for each gene
+#   for(gene in genes){
+#     # select all sites for this gene
+#     tmp <- agR[agR$gene_name==gene,]
+#     
+#     if(nrow(tmp)==1){
+#       next
+#     }
+#     # arrange sites by site position
+#     if(tmp$strand == "-"){
+#       tmp <- dplyr::arrange(tmp, desc(site_pos))
+#     }
+#     else{
+#       tmp <- dplyr::arrange(tmp, site_pos)
+#     }
+#     # assign number based on strand-specific site position
+#     tmp$PtD <- 1:nrow(tmp)
+#     
+#     tmp <- dplyr::select(tmp, txID, PtD)
+#     # add site positions to big list
+#     Temp2 <- rbind(Temp2, tmp)
+#     # grab the most distal site and add to list of most distal sites per gene
+#     MostDistal <- rbind(MostDistal, tmp[nrow(tmp),])
+#   }
+#   # remove first row?
+#   Temp2 <- Temp2[-1,]
+#   MostDistal <- MostDistal[-1,]
+#   
+#   ResultsFull <-  dplyr::full_join(AgaResults, Temp2)
+#   MostDistalFull <-  dplyr::right_join(AgaResults, MostDistal)
+#   
+#   # get all the most proximal sites per gene
+#   ProximalFull <-  dplyr::filter(ResultsFull, PtD == 1)
+#   # get all the following sites
+#   DistalFull <-  dplyr::filter(ResultsFull, PtD != 1)
+#   
+#   return(list(ResultsFull, MostDistalFull, ProximalFull, DistalFull))
+# }
---
+++ scripts/example_walkthrough.R
@@ -0,0 +1,46 @@
+library(dplyr)
+library(ggplot2)
+
+# functions
+
+source("../R/DRIMSeq_functions.R")
+
+data <- as.data.frame(read.table("../example/aga9.expression_sites.tab.gz", sep = "\t", stringsAsFactors=FALSE, header = TRUE) )
+
+# create support file 
+WT <- select(data, contains("FUS.WT"))
+D14 <- select(data, contains("FUS.D14"))
+
+support <- data.frame( 
+  sample_id = c( names(WT), names(D14)),
+  group = factor(c( rep("control", 5), rep("FUS_d14", 5) ), levels = c("control", "FUS_d14"))
+  )
+
+
+# DRIMSeq only wants a file with gene_id, feature_id and then per sample counts
+
+data$feature_id <- paste(data$gene_name, data$site_pos, sep = "_")
+
+sample_counts <- select( data, gene_id, feature_id, contains("FUS.WT"), contains("FUS.D14"))
+
+drimseq_res <- DRIMrun(gr = support, df = sample_counts[1:500,] )
+
+res <- DRIMSeq::results(drimseq_res, level = "feature") %>% 
+  arrange(pvalue)
+
+table(res$adj_pvalue < 0.05)
+
+# plot top gene
+top_gene <- head(res,1)$gene_id
+
+# plot it
+plotPolyA(geneid = top_gene, data = data, DRIMSeq_res = drimseq_res)
+
+# run StageR
+staged_res <- StageRun(drimseq_res,case_condition = "FUS_d14")
+
+# save output
+readr::write_tsv(res,path = "../example/example_results.txt")
+save( drimseq_res, staged_res, file = "../example/example_results.Rdata")
+
+
